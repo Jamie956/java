9798fd329172c602c3d86d16ef8778edbafd78f1
jamie <jamie@justek.us>
2018-04-28 14:48:22 +0800

    update jdbc

--- jdbc/src/main/java/com/example/HelloWorld2.java
+++ jdbc/src/main/java/com/example/HelloWorld2.java
@@ -7,10 +7,18 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class HelloWorld2 {
-	static final String URL = "jdbc:mysql://localhost:3306/mydb?characterEncoding=utf8&useSSL=false";
-	static final String NAME = "root";
-	static final String PASSWORD = "123456";
+	public static void main(String[] args) {
+		test();
+	}
 	
+	private static final String URL = "jdbc:mysql://localhost:3306/mydb?characterEncoding=utf8&useSSL=false";
+	private static final String NAME = "root";
+	private static final String PASSWORD = "123456";
+	
+	/**
+	 * connection to mysql
+	 * @return
+	 */
 	public static Connection getConnection() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -24,21 +32,48 @@ public class HelloWorld2 {
 		return null;
 	}
 	
-	public static void main(String[] args) throws SQLException, ClassNotFoundException {
-		Connection conn = getConnection();
-		Statement stmt = conn.createStatement();
-		String sql = "SELECT * FROM user";
-		ResultSet rs = stmt.executeQuery(sql);
-		while (rs.next()) {
-			System.out.println(rs.getString("id"));
-			System.out.println(rs.getString("name"));
-			System.out.println(rs.getString("email"));
-		}
-		
-		rs.close();
-		stmt.close();
-		conn.close();
-	}
 
+
+	public static void test() {
+		Connection conn = getConnection();
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			stmt = conn.createStatement();
+			String sql = "SELECT * FROM user";
+			rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				System.out.println(rs.getString("id"));
+				System.out.println(rs.getString("name"));
+				System.out.println(rs.getString("email"));
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			
+			if(rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
 
