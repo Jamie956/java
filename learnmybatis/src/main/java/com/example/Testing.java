f64fa4c07c33ebcd9a0613ea90c50453bbadfe2c
jamie956 <752492509@qq.com>
2018-06-13 22:16:44 +0800

    update mybatis

--- learnmybatis/src/main/java/com/example/Testing.java
+++ learnmybatis/src/main/java/com/example/Testing.java
@@ -27,11 +27,21 @@ public class Testing {
 			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
 			session = sqlSessionFactory.openSession();
 
+			// simpleList(session);
+			// simpleInsert(session);
+			// simpleDelete(session);
+			// simpleFindById(session);
+			// simpleUpdate(session);
+//			simpleListByName(session);
+//			simpleListIdByName(session);
+			
+			listOneToMany(session);
+			
 			// findwhere(session);
 			// updateset(session);
-//			findByTrimWhere(session);
+			// findByTrimWhere(session);
 			// updateByTrimSet(session);
-			 listByWhenOtherwise(session);
+			// listByWhenOtherwise(session);
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
@@ -40,189 +50,72 @@ public class Testing {
 		}
 	}
 
-	@Test
-	public void list() {
-		String resource = "mybatis-config.xml";
-		InputStream inputStream = null;
-		SqlSession session = null;
-		try {
-			inputStream = Resources.getResourceAsStream(resource);
-			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-			session = sqlSessionFactory.openSession();
-
-			List<Category> cs = session.selectList("listCategory");
-			for (Category c : cs) {
-				System.out.println(c.getName());
-			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			session.commit();
-			session.close();
-		}
-	}
-
-	@Test
-	public void add() {
-		String resource = "mybatis-config.xml";
-		InputStream inputStream = null;
-		SqlSession session = null;
-		try {
-			inputStream = Resources.getResourceAsStream(resource);
-			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-			session = sqlSessionFactory.openSession();
-
-			Category c = new Category();
-			c.setName("new Category");
-			session.insert("addCategory", c);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			session.commit();
-			session.close();
-		}
-	}
-
-	@Test
-	public void remove() {
-		String resource = "mybatis-config.xml";
-		InputStream inputStream = null;
-		SqlSession session = null;
-		try {
-			inputStream = Resources.getResourceAsStream(resource);
-			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-			session = sqlSessionFactory.openSession();
-
-			Category c = new Category();
-			c.setId(3);
-			session.delete("deleteCategory", c);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			session.commit();
-			session.close();
-		}
-	}
-
-	@Test
-	public void findById() {
-		String resource = "mybatis-config.xml";
-		InputStream inputStream = null;
-		SqlSession session = null;
-		try {
-			inputStream = Resources.getResourceAsStream(resource);
-			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-			session = sqlSessionFactory.openSession();
-
-			Category c = session.selectOne("getCategory", 1);
+	// simple list
+	public static void simpleList(SqlSession session) {
+		List<Category> cs = session.selectList("listCategory0");
+		for (Category c : cs) {
 			System.out.println(c.getName());
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			session.commit();
-			session.close();
 		}
 	}
 
-	@Test
-	public void update() {
-		String resource = "mybatis-config.xml";
-		InputStream inputStream = null;
-		SqlSession session = null;
-		try {
-			inputStream = Resources.getResourceAsStream(resource);
-			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-			session = sqlSessionFactory.openSession();
+	// simple insert
+	public static void simpleInsert(SqlSession session) {
+		Category c = new Category();
+		c.setName("new Category");
+		session.insert("addCategory", c);
+	}
 
-			Category c = session.selectOne("getCategory", 1);
-			c.setName("update category");
-			session.update("updateCategory", c);
+	// simple delete
+	public static void simpleDelete(SqlSession session) {
+		Category c = new Category();
+		c.setId(3);
+		session.delete("deleteCategory", c);
+	}
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			session.commit();
-			session.close();
+	// simple find by id
+	public static void simpleFindById(SqlSession session) {
+		Category c = session.selectOne("getCategory", 1);
+		System.out.println(c.getName());
+	}
+
+	// simple update
+	public static void simpleUpdate(SqlSession session) {
+		Category c = session.selectOne("getCategory", 1);
+		c.setName("update category");
+		session.update("updateCategory", c);
+	}
+
+	// simple list by name
+	public static void simpleListByName(SqlSession session) {
+		List<Category> cs = session.selectList("listCategoryByName", "cat");
+		for (Category c : cs) {
+			System.out.println(c.getName());
 		}
 	}
 
-	@Test
-	public void findByName() {
-		String resource = "mybatis-config.xml";
-		InputStream inputStream = null;
-		SqlSession session = null;
-		try {
-			inputStream = Resources.getResourceAsStream(resource);
-			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-			session = sqlSessionFactory.openSession();
-
-			List<Category> cs = session.selectList("listCategoryByName", "cat");
-			for (Category c : cs) {
-				System.out.println(c.getName());
+//	simple list by id and name
+	public static void simpleListIdByName(SqlSession session) {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("id", 1);
+		params.put("name", "cat");
+		List<Category> cs = session.selectList("listCategoryByIdAndName", params);
+		for (Category c : cs) {
+			System.out.println(c.getName());
+		}
+	}
+	
+//	list one to many
+	public static void listOneToMany(SqlSession session) {
+		List<Category> cs = session.selectList("listCategory2");
+		for (Category c : cs) {
+			System.out.println(c);
+			List<Product> ps = c.getProducts();
+			for (Product p : ps) {
+				System.out.println("\t" + p);
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			session.commit();
-			session.close();
 		}
 	}
-
-	@Test
-	public void findByIdAndName() {
-		String resource = "mybatis-config.xml";
-		InputStream inputStream = null;
-		SqlSession session = null;
-		try {
-			inputStream = Resources.getResourceAsStream(resource);
-			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-			session = sqlSessionFactory.openSession();
-
-			Map<String, Object> params = new HashMap<String, Object>();
-			params.put("id", 1);
-			params.put("name", "cat");
-			List<Category> cs = session.selectList("listCategoryByIdAndName", params);
-			for (Category c : cs) {
-				System.out.println(c.getName());
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			session.commit();
-			session.close();
-		}
-	}
-
-	@Test
-	public void listOntToMany() {
-		String resource = "mybatis-config.xml";
-		InputStream inputStream = null;
-		SqlSession session = null;
-		try {
-			inputStream = Resources.getResourceAsStream(resource);
-			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-			session = sqlSessionFactory.openSession();
-
-			List<Category> cs = session.selectList("listCategory");
-			for (Category c : cs) {
-				System.out.println(c);
-				List<Product> ps = c.getProducts();
-				for (Product p : ps) {
-					System.out.println("\t" + p);
-				}
-			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			session.commit();
-			session.close();
-		}
-	}
+	
 
 	@Test
 	public void manytoone() {
@@ -418,8 +311,8 @@ public class Testing {
 	// list by when-otherwise tag
 	public static void listByWhenOtherwise(SqlSession session) {
 		Map<String, Object> params = new HashMap<String, Object>();
-//		 params.put("name","a");
-//		 params.put("price","10");
+		// params.put("name","a");
+		// params.put("price","10");
 		List<Product> ps = session.selectList("listProduct5", params);
 		for (Product p : ps) {
 			System.out.println(p);
