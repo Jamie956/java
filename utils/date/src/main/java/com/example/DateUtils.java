24ebc2d728d4368ef95462afaaae077b8e79aee1
Unknown <752492509@qq.com>
2018-07-13 08:05:47 +0800

    update

--- utils/date/src/main/java/com/example/DateUtils.java
+++ utils/date/src/main/java/com/example/DateUtils.java
@@ -8,48 +8,56 @@ import java.util.List;
 
 public class DateUtils {
 	
+	//获取指定日期的年份
 	public static int getYear(Date date) {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(date);
 		return cal.get(Calendar.YEAR);
 	}
 	
+	//获取指定日期的月份
 	public static int getMonth(Date date) {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(date);
 		return cal.get(Calendar.MONTH)+1;
 	}
 	
+	//获取指定日期一个月的第几天
 	public static int getDay(Date date) {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(date);
 		return cal.get(Calendar.DAY_OF_MONTH);
 	}
 	
+	//获取指定日期一年的第几周
 	public static int getWeekOfYear(Date date) {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(date);
 		return cal.get(Calendar.WEEK_OF_YEAR);
 	}
 	
+	//获取指定日期一年的第几天
 	public static int getDayOfYear(Date date) {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(date);
 		return cal.get(Calendar.DAY_OF_YEAR);
 	}
 	
+	//获取指定日期一月的第几周
 	public static int getWeekOfMonth(Date date) {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(date);
 		return cal.get(Calendar.WEEK_OF_MONTH);
 	}
 	
+	//获取指定日期一周的第几天
 	public static int getDayOfWeek(Date date) {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(date);
 		return cal.get(Calendar.DAY_OF_WEEK);
 	}
 	
+	//获取过去x天的日期
 	public static Date getLastDays(int days) {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(new Date());
@@ -57,6 +65,7 @@ public class DateUtils {
 		return cal.getTime();
 	}
 	
+	//获取将来x天的日期
 	public static Date getNextDays(int days) {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(new Date());
@@ -64,6 +73,7 @@ public class DateUtils {
 		return cal.getTime();
 	}
 
+	//获取两个日期相隔多少天
 	public static int getPeriodOfDays(Date from, Date to) {
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(from);
@@ -73,7 +83,8 @@ public class DateUtils {
 		long periodOfDays = (endTime - startTime) / (1000 * 3600 * 24);
 		return Integer.parseInt(String.valueOf(periodOfDays));
 	}
-
+	
+	//获取一段时间内的所有日期
 	public static List<Date> getContinuousDate(Date from, Date to) {
 		int periodOfDays = getPeriodOfDays(from, to) + 1;
 
