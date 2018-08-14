98475fc47f9f8a3b050c27c62a1b409e1b3bb109
Unknown <752492509@qq.com>
2018-08-15 07:55:19 +0800

    up

--- javanotes/src/main/java/com/example/design/EagerSingleton.java
+++ javanotes/src/main/java/com/example/design/EagerSingleton.java
@@ -4,17 +4,21 @@ public class EagerSingleton {
 	public static void main(String[] args) {
 		Singleton2 s1 = Singleton2.getInstance();
 		Singleton2 s2 = Singleton2.getInstance();
-		System.out.println(s1 == s2);// ture
-
+		System.out.println(s1 == s2);// ture,对象地址一样,是同一个对象
+		System.out.println(s1.hashCode());
+		System.out.println(s2.hashCode());
 	}
 }
 
+//单例
 class Singleton2 {
-	private static final Singleton2 instance2 = new Singleton2();// create Object when class load
+	//类加载时创建对象
+	private static final Singleton2 instance2 = new Singleton2();
 
 	private Singleton2() {
 	}
-
+	
+	//返回类加载时创建的那个对象
 	public static Singleton2 getInstance() {
 		return instance2;
 	}
