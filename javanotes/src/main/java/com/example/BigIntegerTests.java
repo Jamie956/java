c60a0cdbf830c1a96c97983f5d79bb03b5994614
jamie <jamie@justek.us>
2018-08-15 11:54:12 +0800

    up

--- javanotes/src/main/java/com/example/BigIntegerTests.java
+++ javanotes/src/main/java/com/example/BigIntegerTests.java
@@ -1,10 +1,11 @@
 package com.example;
 
 import java.math.BigInteger;
+import java.security.interfaces.RSAKey;
 
 public class BigIntegerTests {
 	public static void main(String[] args) {
-		test01();
+		test03();
 	}
 	public static void test01() {
 		long a = Long.MAX_VALUE;
@@ -23,5 +24,62 @@ public class BigIntegerTests {
 		String binaryString = "110";
 		rs = new BigInteger(binaryString , 2);
 		System.out.println(rs);
+		
+		rs = BigInteger.ZERO;
+		rs = BigInteger.ONE;
+		rs = BigInteger.TEN;
+	}
+	
+	public static void test02() {
+		//算术
+		BigInteger a = new BigInteger("10");
+		BigInteger b = new BigInteger("10");
+		//+
+		BigInteger rs = a.add(b);
+		System.out.println(rs);
+		//-
+		rs = a.subtract(b);
+		System.out.println(rs);
+		// /
+		rs = a.divide(b);
+		System.out.println(rs);
+		//*
+		rs = a.multiply(b);
+		System.out.println(rs);
+		// ^
+		rs = a.pow(3);
+		System.out.println(rs);
+		// %
+		rs = a.remainder(b);
+		System.out.println(rs);
+		//最大公约数
+		rs = a.gcd(b);
+		System.out.println(rs);
+		//最大值
+		rs = a.max(b);
+		System.out.println(rs);
+		//最小值
+		rs = a.min(b);
+		System.out.println(rs);
+	}
+	
+	public static void test03() {
+		BigInteger a = BigInteger.valueOf(1);
+		BigInteger b = BigInteger.valueOf(2);
+		//大小比较
+		a.equals(b);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
