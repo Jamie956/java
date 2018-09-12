c0976d50aedcc1127c4ba6df6e5080adbff10ab1
jamie <jamie@justek.us>
2018-09-12 11:40:41 +0800

    up

---
+++ api-gateway/api-gateway-service/src/main/java/com/example/demo/MobileProduct.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class MobileProduct {
+	private String price;
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+}
