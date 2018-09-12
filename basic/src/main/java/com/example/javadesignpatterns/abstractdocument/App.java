03d305ad3f65443b1fe6e89b9e7357aa24cb9e39
jamie <jamie@justek.us>
2018-09-12 10:22:42 +0800

    up

--- basic/src/main/java/com/example/javadesignpatterns/abstractdocument/App.java
+++ basic/src/main/java/com/example/javadesignpatterns/abstractdocument/App.java
@@ -6,7 +6,7 @@ import com.example.javadesignpatterns.abstractdocument.domain.*;
 
 public class App {
 	public static void main(String[] args) {
-		test02();
+		test03();
 	}
 
 	public static void test01() {
@@ -32,64 +32,8 @@ public class App {
 		System.out.println(car.getPrice().get());
 		System.out.println(car.getParts().count());
 	}
-	
-	
 
-///**
-// * AbstractDocument test class
-// */
-//public class AbstractDocumentTest {
-//
-//  private static final String KEY = "key";
-//  private static final String VALUE = "value";
-//
-//  private class DocumentImplementation extends AbstractDocument {
-//
-//    DocumentImplementation(Map<String, Object> properties) {
-//      super(properties);
-//    }
-//  }
-//
-//  private DocumentImplementation document = new DocumentImplementation(new HashMap<>());
-//
-//  @Test
-//  public void shouldPutAndGetValue() {
-//    document.put(KEY, VALUE);
-//    assertEquals(VALUE, document.get(KEY));
-//  }
-//
-//  @Test
-//  public void shouldRetrieveChildren() {
-//    Map<String, Object> child1 = new HashMap<>();
-//    Map<String, Object> child2 = new HashMap<>();
-//    List<Map<String, Object>> children = Arrays.asList(child1, child2);
-//
-//    document.put(KEY, children);
-//
-//    Stream<DocumentImplementation> childrenStream = document.children(KEY, DocumentImplementation::new);
-//    assertNotNull(children);
-//    assertEquals(2, childrenStream.count());
-//  }
-//
-//  @Test
-//  public void shouldRetrieveEmptyStreamForNonExistingChildren() {
-//    Stream<DocumentImplementation> children = document.children(KEY, DocumentImplementation::new);
-//    assertNotNull(children);
-//    assertEquals(0, children.count());
-//  }
-//
-//  @Test
-//  public void shouldIncludePropsInToString() {
-//    Map<String, Object> props = new HashMap<>();
-//    props.put(KEY, VALUE);
-//    DocumentImplementation document = new DocumentImplementation(props);
-//    assertNotNull(document.toString().contains(KEY));
-//    assertNotNull(document.toString().contains(VALUE));
-//  }
-//
-//}
-	
-	public static void testx() {
+	public static void test03() {
 		Map<String, Object> carProperties = new HashMap<>();
 		carProperties.put(HasModel.PROPERTY, "300SL");
 		carProperties.put(HasPrice.PROPERTY, 10000L);
@@ -113,5 +57,4 @@ public class App {
 				p -> System.out.println(p.getType().get() + " - " + p.getModel().get() + " - " + p.getPrice().get()));
 
 	}
-
 }
