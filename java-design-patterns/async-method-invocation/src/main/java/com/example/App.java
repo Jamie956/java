881eef9250a397baaebcb2585fc5a3d4f7ddc8d3
jamie <jamie@justek.us>
2018-09-14 11:15:57 +0800

    up

--- java-design-patterns/async-method-invocation/src/main/java/com/example/App.java
+++ java-design-patterns/async-method-invocation/src/main/java/com/example/App.java
@@ -4,45 +4,63 @@ import java.util.concurrent.Callable;
 
 public class App {
 	public static void main(String[] args) throws Exception {
-		test01();
-	}
-
-	public static void mainx(String[] args) throws Exception {
-		AsyncExecutor executor = new ThreadAsyncExecutor();
-
-		AsyncResult<Integer> asyncResult1 = executor.startProcess(lazyval(10, 500));
-		AsyncResult<String> asyncResult2 = executor.startProcess(lazyval("test", 300));
-		AsyncResult<Long> asyncResult3 = executor.startProcess(lazyval(50L, 700));
-		AsyncResult<Integer> asyncResult4 = executor.startProcess(lazyval(20, 400), callback("Callback result 4"));
-		AsyncResult<String> asyncResult5 = executor.startProcess(lazyval("callback", 600),
-				callback("Callback result 5"));
-
-		Thread.sleep(350); // Oh boy I'm working hard here
-		System.out.println("Some hard work done");
-
-		Integer result1 = executor.endProcess(asyncResult1);
-		String result2 = executor.endProcess(asyncResult2);
-		Long result3 = executor.endProcess(asyncResult3);
-		asyncResult4.await();
-		asyncResult5.await();
-
-		System.out.println("Result 1: " + result1);
-		System.out.println("Result 2: " + result2);
-		System.out.println("Result 3: " + result3);
+		test04();
 	}
 
 	public static void test01() throws Exception {
 		AsyncExecutor executor = new ThreadAsyncExecutor();
-
-		AsyncResult<Integer> asyncResult1 = executor.startProcess(lazyval(10, 500));
-
+		AsyncResult<Integer> asyncRs = executor.startProcess(lazyval(10, 500));
+		
 		Thread.sleep(350);
 		System.out.println("Some hard work done");
 
-		Integer result1 = executor.endProcess(asyncResult1);
-		System.out.println("Result 1: " + result1);
+		Integer rs = executor.endProcess(asyncRs);
+		System.out.println("Result: " + rs);
 	}
+	
+	public static void test02() throws Exception {
+		AsyncExecutor executor = new ThreadAsyncExecutor();
+		AsyncResult<String> asyncRs = executor.startProcess(lazyval("test", 300));
+		
+		Thread.sleep(350);
+		System.out.println("Some hard work done");
 
+		String rs = executor.endProcess(asyncRs);
+		System.out.println("Result: " + rs);
+	}
+	
+	public static void test03() throws Exception {
+		AsyncExecutor executor = new ThreadAsyncExecutor();
+		AsyncResult<Long> asyncRs = executor.startProcess(lazyval(50L, 700));
+		
+		Thread.sleep(350);
+		System.out.println("Some hard work done");
+
+		Long rs = executor.endProcess(asyncRs);
+		System.out.println("Result: " + rs);
+	}
+	
+	public static void test04() throws Exception {
+		AsyncExecutor executor = new ThreadAsyncExecutor();
+		AsyncResult<Integer> asyncRs = executor.startProcess(lazyval(20, 400), callback("Callback result 4"));
+		
+		Thread.sleep(350);
+		System.out.println("Some hard work done");
+
+		asyncRs.await();
+	}
+	
+	public static void test05() throws Exception {
+		AsyncExecutor executor = new ThreadAsyncExecutor();
+		AsyncResult<String> asyncRs = executor.startProcess(lazyval("callback", 600), callback("Callback result 5"));
+		
+		Thread.sleep(350);
+		System.out.println("Some hard work done");
+		
+		asyncRs.await();
+	}
+	
+	// Return Callable function that return the value
 	private static <T> Callable<T> lazyval(T value, long delayMillis) {
 		return () -> {
 			Thread.sleep(delayMillis);
