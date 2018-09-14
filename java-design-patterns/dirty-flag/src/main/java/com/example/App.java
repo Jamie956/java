41a3d0367908c496191b1bb6e2cc23992c831f77
jamie <jamie@justek.us>
2018-09-14 18:21:36 +0800

    up

---
+++ java-design-patterns/dirty-flag/src/main/java/com/example/App.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class App {
+	public void run() {
+
+		final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+		executorService.scheduleAtFixedRate(new Runnable() {
+			@Override
+			public void run() {
+				World world = new World();
+				List<String> countries = world.fetch();
+				System.out.println("Our world currently has the following countries:-");
+				for (String country : countries) {
+					System.out.println("\t" + country);
+				}
+			}
+		}, 0, 15, TimeUnit.SECONDS); // Run at every 15 seconds.
+	}
+
+	public static void main(String[] args) {
+		App app = new App();
+
+		app.run();
+	}
+}
---
+++ java-design-patterns/dirty-flag/src/main/java/com/example/World.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class World {
+	private List<String> countries;
+	private DataFetcher df;
+
+	public World() {
+		this.countries = new ArrayList<String>();
+		this.df = new DataFetcher();
+	}
+
+	public List<String> fetch() {
+		List<String> data = df.fetch();
+
+		countries = data.isEmpty() ? countries : data;
+
+		return countries;
+	}
+}
