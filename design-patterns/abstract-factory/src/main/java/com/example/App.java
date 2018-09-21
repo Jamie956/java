6af5f83dd2ec5103a6433fb95b43bfb636fe9aef
jamie <jamie@justek.us>
2018-09-21 17:35:07 +0800

    up

--- design-patterns/abstract-factory/src/main/java/com/example/App.java
+++ design-patterns/abstract-factory/src/main/java/com/example/App.java
@@ -1,17 +1,31 @@
 package com.example;
 
+import com.example.entity.Army;
+import com.example.entity.Castle;
+import com.example.entity.King;
+import com.example.factory.ElfKingdomFactory;
+
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Elf Kingdom");
+		test02();
+	}
+
+	public static void test01() {
+		ElfKingdomFactory elfKingdomFactory = new ElfKingdomFactory();
+		Army elfArmy = elfKingdomFactory.createArmy();
+		Castle elfCastle = elfKingdomFactory.createCastle();
+		King elfKing = elfKingdomFactory.createKing();
+
+		System.out.println(elfArmy.getDescription());
+		System.out.println(elfCastle.getDescription());
+		System.out.println(elfKing.getDescription());
+	}
+
+	public static void test02() {
 		CreateKingdom kd = new CreateKingdom(FactoryMaker.create(KingdomType.ELF));
+		
 		System.out.println(kd.getArmy().getDescription());
 		System.out.println(kd.getCastle().getDescription());
 		System.out.println(kd.getKing().getDescription());
-
-		System.out.println("Orc Kingdom");
-		CreateKingdom kd2 = new CreateKingdom(FactoryMaker.create(KingdomType.ORC));
-		System.out.println(kd2.getArmy().getDescription());
-		System.out.println(kd2.getCastle().getDescription());
-		System.out.println(kd2.getKing().getDescription());
 	}
 }
