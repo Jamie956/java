6f47838db9c88bfa6c96ab41ef0eb029e35f3cf7
Unknown <752492509@qq.com>
2018-09-22 23:11:42 +0800

    up

--- design-patterns/builder/src/main/java/com/example/Hero.java
+++ design-patterns/builder/src/main/java/com/example/Hero.java
@@ -8,7 +8,7 @@ public final class Hero {
 	private final Armor armor;
 	private final Weapon weapon;
 
-	private Hero(Builder builder) {
+	Hero(HeroBuilder builder) {
 		this.profession = builder.profession;
 		this.name = builder.name;
 		this.hairColor = builder.hairColor;
@@ -43,66 +43,7 @@ public final class Hero {
 
 	@Override
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("This is a ").append(profession).append(" named ").append(name);
-		if (hairColor != null || hairType != null) {
-			sb.append(" with ");
-			if (hairColor != null) {
-				sb.append(hairColor).append(' ');
-			}
-			if (hairType != null) {
-				sb.append(hairType).append(' ');
-			}
-			sb.append(hairType != HairType.BALD ? "hair" : "head");
-		}
-		if (armor != null) {
-			sb.append(" wearing ").append(armor);
-		}
-		if (weapon != null) {
-			sb.append(" and wielding a ").append(weapon);
-		}
-		sb.append('.');
-		return sb.toString();
-	}
-
-	public static class Builder {
-		private final Profession profession;
-		private final String name;
-		private HairType hairType;
-		private HairColor hairColor;
-		private Armor armor;
-		private Weapon weapon;
-
-		public Builder(Profession profession, String name) {
-			if (profession == null || name == null) {
-				throw new IllegalArgumentException("profession and name can not be null");
-			}
-			this.profession = profession;
-			this.name = name;
-		}
-
-		public Builder withHairType(HairType hairType) {
-			this.hairType = hairType;
-			return this;
-		}
-
-		public Builder withHairColor(HairColor hairColor) {
-			this.hairColor = hairColor;
-			return this;
-		}
-
-		public Builder withArmor(Armor armor) {
-			this.armor = armor;
-			return this;
-		}
-
-		public Builder withWeapon(Weapon weapon) {
-			this.weapon = weapon;
-			return this;
-		}
-
-		public Hero build() {
-			return new Hero(this);
-		}
+		return "Hero [profession=" + profession + ", name=" + name + ", hairType=" + hairType + ", hairColor="
+				+ hairColor + ", armor=" + armor + ", weapon=" + weapon + "]";
 	}
 }
