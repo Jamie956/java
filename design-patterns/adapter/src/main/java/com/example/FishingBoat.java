cf814915152cfb3cec9b296eac815bda29c9991e
jamie <jamie@justek.us>
2018-09-25 16:06:57 +0800

    up

--- design-patterns/adapter/src/main/java/com/example/FishingBoat.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class FishingBoat {
-	public void sail() {
-		System.out.println("The fishing boat is sailing");
-	}
-}
--- design-patterns/adapter/src/main/java/com/example/RowingBoat.java
+++
@@ -1,5 +0,0 @@
-package com.example;
-
-public interface RowingBoat {
-	void row();
-}
