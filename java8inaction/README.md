af265e25225726ec7b8a246d476eae0b33c04975
jamie <jamie@justek.us>
2018-10-09 11:04:05 +0800

    up

--- java8inaction/README.md
+++ java8inaction/README.md
@@ -4,3 +4,32 @@
 Runnable r = () -> System.out.println("Hello!");
 r.run();
 ```
+
+### Stream
+```java
+//String to Stream
+Stream.of("Java 8", "Lambdas", "In", "Action");
+//Stream map
+stream.map(String::toUpperCase);
+//Stream limit
+stream = stream.limit(2);
+//Stream forEach
+stream.forEach(System.out::println);
+//Create empty stream
+Stream.empty();
+
+//Stream iterate
+Stream.iterate(0, n -> n + 2);
+//Stream generate
+Stream.generate(Math::random);
+
+//Array to IntStream
+Arrays.stream(array);
+//IntStream sum
+intStream.sum();
+
+
+
+
+
+```
--- java8inaction/src/main/java/com/example/ch05/IterateStream.java
+++ java8inaction/src/main/java/com/example/ch05/IterateStream.java
@@ -1,7 +1,6 @@
 package com.example.ch05;
 
 import java.util.*;
-import java.util.function.IntSupplier;
 import java.util.stream.*;
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -9,72 +8,47 @@ import java.nio.file.*;
 
 public class IterateStream {
 	public static void main(String[] args) throws IOException {
-		test11();
+		test05();
 	}
-
+	
 	public static void test01() {
-		Stream.of("Java 8", "Lambdas", "In", "Action").map(String::toUpperCase).forEach(System.out::println);
-	}
-
-	public static void test02() {
+		//String to Stream
+		Stream<String> stream = Stream.of("Java 8", "Lambdas", "In", "Action");
+		//Stream map
+		stream = stream.map(String::toUpperCase);
+		//Stream limit
+		stream = stream.limit(2);
+		//Stream forEach
+		stream.forEach(System.out::println);
+		
+		//Create empty stream
 		Stream.empty();
 	}
-
-	public static void test03() {
-		int rs = Arrays.stream(new int[] { 2, 3, 5, 7, 11, 13 }).sum();
+	
+	public static void test02() {
+		//Array to IntStream
+		IntStream intStream = Arrays.stream(new int[] { 2, 3, 5, 7, 11, 13 });
+		//IntStream sum
+		int rs = intStream.sum();
 		System.out.println(rs);
 	}
 
-	public static void test04() {
+	public static void test03() {
+		//Stream iterate
 		Stream.iterate(0, n -> n + 2).limit(10).forEach(System.out::println);
 	}
 
-	public static void test05() {
-		Stream.iterate(new int[] { 0, 1 }, t -> new int[] { t[1], t[0] + t[1] }).limit(10)
-				.forEach(t -> System.out.println("(" + t[0] + ", " + t[1] + ")"));
-	}
-
-	public static void test06() {
-		Stream.iterate(new int[] { 0, 1 }, t -> new int[] { t[1], t[0] + t[1] }).limit(10).map(t -> t[0])
-				.forEach(System.out::println);
-	}
-
-	public static void test07() {
+	public static void test04() {
+		//Stream generate
 		Stream.generate(Math::random).limit(10).forEach(System.out::println);
-	}
-
-	public static void test08() {
+		//IntStream generate
 		IntStream.generate(() -> 1).limit(5).forEach(System.out::println);
 	}
 
-	public static void test09() {
-		IntStream.generate(new IntSupplier() {
-			public int getAsInt() {
-				return 2;
-			}
-		}).limit(5).forEach(System.out::println);
-	}
-
-	public static void test10() {
-		IntSupplier fib = new IntSupplier() {
-			private int previous = 0;
-			private int current = 1;
-
-			public int getAsInt() {
-				int nextValue = this.previous + this.current;
-				this.previous = this.current;
-				this.current = nextValue;
-				return this.previous;
-			}
-		};
-		IntStream.generate(fib).limit(10).forEach(System.out::println);
-	}
-
-	public static void test11() throws IOException {
+	public static void test05() throws IOException {
 		long uniqueWords = Files
 				.lines(Paths.get("src/main/java/com/example/ch05/data.txt"), Charset.defaultCharset())
 				.flatMap(line -> Arrays.stream(line.split(" "))).distinct().count();
-
 		System.out.println("There are " + uniqueWords + " unique words in data.txt");
 	}
 }
