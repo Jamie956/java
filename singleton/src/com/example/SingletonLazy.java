f305c4a921c06476f5acbe4a619e96c68d81d4ec
Jamie <752492509@qq.com>
2019-01-23 17:32:25 +0800

    up

--- singleton/src/com/example/SingletonLazy.java
+++ singleton/src/com/example/SingletonLazy.java
@@ -4,9 +4,8 @@ public class SingletonLazy {
 	private static SingletonLazy uniqueInstance;
 	private SingletonLazy() {
 	}
-	// 没有加入synchronized关键字的版本是线程不安全的
+	// synchronized确保线程安全
 	public static synchronized SingletonLazy getInstance() {
-		// 判断当前单例是否已经存在，若存在则返回，不存在则再建立单例
 		if (uniqueInstance == null) {
 			uniqueInstance = new SingletonLazy();
 		}
