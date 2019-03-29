39b67c057dc1849b51477bd7ef2a5ef56767e3cf
Jamie <752492509@qq.com>
2019-03-29 17:15:03 +0800

    up

--- basic/src/com/example/linkedlist/LinkedList.java
+++ basic/src/com/example/linkedlist/LinkedList.java
@@ -77,5 +77,25 @@ public class LinkedList {
 
 		prev.next = temp.next;
 	}
+	
+	public static void main(String[] args) {
+		LinkedList llist = new LinkedList();
 
+		llist.head = new Node(1);
+		Node second = new Node(2);
+		Node third = new Node(3);
+		llist.head.next = second;
+		second.next = third;
+
+		llist.printList();
+		
+		llist.push(4);
+		llist.printList();
+		
+		llist.append(5);
+		llist.printList();
+		
+		llist.deleteNode(2);
+		llist.printList();
+	}
 }
