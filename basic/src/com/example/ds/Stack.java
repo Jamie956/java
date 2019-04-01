276e35a1fa7440ab942c3869c4dc8571355f460c
Jamie <752492509@qq.com>
2019-04-01 11:54:20 +0800

    up

---
+++ basic/src/com/example/ds/Stack.java
@@ -0,0 +1,46 @@
+package com.example.ds;
+
+public class Stack {
+	static final int MAX = 1000;
+	int top;
+	int a[] = new int[MAX];
+
+	Stack() {
+		top = -1;
+	}
+	
+	boolean isEmpty() {
+		return (top < 0);
+	}
+
+
+	boolean push(int x) {
+		if (top >= (MAX - 1)) {
+			System.out.println("Stack Overflow");
+			return false;
+		} else {
+			a[++top] = x;
+			System.out.println(x + " pushed into stack");
+			return true;
+		}
+	}
+
+	int pop() {
+		if (top < 0) {
+			System.out.println("Stack Underflow");
+			return 0;
+		} else {
+			int x = a[top--];
+			System.out.println(x + " Popped from stack");
+			return x;
+		}
+	}
+
+	public static void main(String args[]) {
+		Stack s = new Stack();
+		s.push(10);
+		s.push(20);
+		s.push(30);
+		s.pop();
+	}
+}
