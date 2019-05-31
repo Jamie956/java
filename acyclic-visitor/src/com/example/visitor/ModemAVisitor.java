ecc9b1ed0a25c9cc72c54afc958b07ff26422d20
Jamie <752492509@qq.com>
2019-05-31 17:21:38 +0800

    up

--- acyclic-visitor/src/com/example/visitor/ModemAVisitor.java
+++
@@ -1,7 +0,0 @@
-package com.example.visitor;
-
-import com.example.modem.ModemA;
-
-public interface ModemAVisitor extends ModemVisitor {
-  void visit(ModemA modemA);
-}
--- acyclic-visitor/src/com/example/visitor/ModemBVisitor.java
+++
@@ -1,7 +0,0 @@
-package com.example.visitor;
-
-import com.example.modem.ModemB;
-
-public interface ModemBVisitor extends ModemVisitor {
-  void visit(ModemB modemB);
-}
