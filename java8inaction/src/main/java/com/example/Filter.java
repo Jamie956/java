46525c486142a7a24d2c69439f5c058273f91368
Jamie <752492509@qq.com>
2019-06-05 11:14:10 +0800

    up

---
+++ java8inaction/src/main/java/com/example/Filter.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import java.util.function.Predicate;
+
+public class Filter {
+	public static void main(String[] args) {
+		test02();
+	}
+
+	private static boolean isGreenApple(Color color) {
+		return "green".equals(color.getName());
+	}
+	
+	public static void test01() {
+		Predicate<Color> predicate = Filter::isGreenApple;
+		
+		System.out.println(predicate.test(new Color("red")));
+		System.out.println(predicate.test(new Color("green")));
+	}
+
+	public static void test02() {
+		Predicate<Color> predicate = (Color color) -> "green".equals(color.getName());
+		
+		System.out.println(predicate.test(new Color("green")));
+		System.out.println(predicate.test(new Color("red")));
+	}
+
+}
