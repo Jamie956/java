cc37047385858372ba8e8ec6d5652192adad9010
Jamie <752492509@qq.com>
2019-06-05 12:01:55 +0800

    up

--- java8inaction/src/main/java/com/example/StreamConvert.java
+++ java8inaction/src/main/java/com/example/StreamConvert.java
@@ -6,12 +6,13 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class Stream_List {
+public class StreamConvert {
 	public static void main(String[] args) {
 		test02();
 	}
 	
 	public static void test01() {
+		// String to List
 		List<String> list = Arrays.asList("a", "b", "c");
 		//List -> stream
 		Stream<String> stream = list.stream();
@@ -19,6 +20,7 @@ public class Stream_List {
 	}
 	
 	public static void test02() {
+		// String to Stream
 		Stream<String> stream = Stream.of("a", "b", "c");
 		// Stream -> List
 		List<String> streamList = stream.collect(toList());
