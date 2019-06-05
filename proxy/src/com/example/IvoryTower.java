94ef58c23ad40a2cbbb789410017f8090975b249
Jamie <752492509@qq.com>
2019-06-05 10:30:54 +0800

    up

--- proxy/src/com/example/IvoryTower.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public class IvoryTower implements WizardTower {
-
-	public void enter(Wizard wizard) {
-		System.out.println(wizard + " enters the tower.");
-	}
-
-}
--- proxy/src/com/example/WizardTower.java
+++
@@ -1,6 +0,0 @@
-package com.example;
-
-public interface WizardTower {
-
-	void enter(Wizard wizard);
-}
