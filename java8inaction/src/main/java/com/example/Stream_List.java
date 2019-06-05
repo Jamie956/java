4547e7a445e179fc40c22de664f7994ab60e6d35
Jamie <752492509@qq.com>
2019-06-05 12:00:18 +0800

    up

--- java8inaction/src/main/java/com/example/Stream_List.java
+++ java8inaction/src/main/java/com/example/Stream_List.java
@@ -7,20 +7,19 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class Stream_List {
-	private static List<String> list = Arrays.asList("a", "b", "c");
-	
 	public static void main(String[] args) {
 		test02();
 	}
 	
 	public static void test01() {
+		List<String> list = Arrays.asList("a", "b", "c");
 		//List -> stream
 		Stream<String> stream = list.stream();
 		stream.forEach(System.out::println);
 	}
 	
 	public static void test02() {
-		Stream<String> stream = list.stream();
+		Stream<String> stream = Stream.of("a", "b", "c");
 		// Stream -> List
 		List<String> streamList = stream.collect(toList());
 		streamList.forEach(System.out::println);
