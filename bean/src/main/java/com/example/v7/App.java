0d4856a02078473abd5b2eeda832bfe0a228691f
Jamie <752492509@qq.com>
2019-06-06 17:12:15 +0800

    up

--- bean/src/main/java/com/example/v7/App.java
+++ bean/src/main/java/com/example/v7/App.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.example.v7;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-		context.scan("com.example");
+		context.scan("com.example.v7");
 		context.refresh();
 		
 		User u = context.getBean(User.class);
