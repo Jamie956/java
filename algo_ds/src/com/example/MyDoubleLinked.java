90027c04de05bf557108cf0d3f7785f1c0450804
Jamie956 <752492509@qq.com>
2020-05-15 20:44:15 +0800

    add link

---
+++ algo_ds/src/com/example/MyDoubleLinked.java
@@ -0,0 +1,70 @@
+package com.example;
+
+public class MyDoubleLinked<E> {
+    transient int size = 0;
+    transient Node<E> first;
+    transient Node<E> last;
+
+    private static class Node<E> {
+        Node<E> prev;
+        Node<E> next;
+        E item;
+
+        Node(Node<E> prev, E item, Node<E> next) {
+            this.prev = prev;
+            this.next = next;
+            this.item = item;
+        }
+    }
+
+    public boolean add(E e) {
+        final Node<E> l = last;
+        final Node newNode = new Node<E>(last, e, null);
+
+        last = newNode;
+        if (l == null) {
+            first = newNode;
+        } else {
+            newNode.prev = l;
+            l.next = newNode;
+        }
+        size++;
+        return true;
+    }
+
+    public void remove(E e) {
+
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException("index: " + index + " size: " + size);
+
+        if (index < (size >> 1)) {
+            Node<E> h = first;
+            for (int i = 0; i < index; i++) {
+                h = h.next;
+            }
+            return h.item;
+        } else {
+            Node<E> t = last;
+            for (int i = size - 1; i > index; i--) {
+                t = t.prev;
+            }
+            return t.item;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyDoubleLinked link = new MyDoubleLinked();
+        link.add(1);
+        link.add(2);
+        link.add(3);
+        link.add(4);
+
+        link.get(0);
+        link.get(3);
+        link.get(6);
+    }
+
+
+}
