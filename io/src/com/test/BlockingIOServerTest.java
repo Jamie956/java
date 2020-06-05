aa16d80432687133949b4cb4f993234b0be58bb3
Jamie956 <752492509@qq.com>
2020-06-05 21:11:06 +0800

    update io

---
+++ io/src/com/test/BlockingIOServerTest.java
@@ -0,0 +1,28 @@
+package com.test;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class BlockingIOServerTest {
+    public static void main(String[] args) {
+        try {
+            ServerSocket ss = new ServerSocket(8081);
+
+            while (true) {
+                Socket socket = ss.accept();
+                OutputStream out = socket.getOutputStream();
+                out.write("hi".getBytes());
+
+                out.close();
+                socket.close();
+            }
+
+//            ss.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
