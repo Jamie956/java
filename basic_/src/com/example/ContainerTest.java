91676c33b86222a391de34cae67093caa1f96d5f
Jamie956 <752492509@qq.com>
2020-07-04 16:27:27 +0800

    add container test

--- basic_/src/com/example/ContainerTest.java
+++ basic_/src/com/example/ContainerTest.java
@@ -1,8 +1,9 @@
 package com.example;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
+import sun.reflect.generics.tree.Tree;
+
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class ContainerTest {
 
@@ -82,7 +83,72 @@ public class ContainerTest {
         new LinkedList();
     }
 
+    /**
+     * linked list CRUD test
+     * 1.指针连接
+     * 2.节点数据结构，前后双指针
+     * 3.移除，断开连接
+     */
+    public static void test6() {
+        LinkedList list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+
+        list.remove();
+    }
+
+    /**
+     * Set 构造实验
+     * 1.hashmap存储元素
+     * 2.无参，构造函数初始默认load factor
+     * 3.集合参数，size小于12时使用默认容量16
+     * 4.容量，加载因子参数
+     */
+    public static void test7() {
+        new HashSet<>();
+
+        int[] a = {1, 2, 3};
+        List list = Arrays.asList(a);
+        new HashSet<>(list);
+
+        new HashSet<>(12, 0.8f);
+    }
+
+    /**
+     * set add element test
+     *
+     */
+    public static void test8(){
+        HashSet set = new HashSet();
+        set.add(1);
+        set.add(2);
+        set.add(1);
+    }
+
+    /**
+     * tree set 构造函数测试
+     * 1.创建tree map
+     */
+    public static void test9(){
+        new TreeSet<>();
+
+    }
+
+    /**
+     * hash map test
+     * 1.构造函数初始load factor
+     * 2.元素储存在table数组
+     * 3.节点结构，hash, key, val, next
+     */
+    public static void test10(){
+        HashMap map = new HashMap<>();
+        map.put("k1","v1");
+        map.put("k2","v2");
+    }
+
+
+
     public static void main(String[] args) {
-        test5();
+        test10();
     }
 }
