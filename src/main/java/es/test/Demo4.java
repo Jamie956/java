6b992ad0787210234da216231f2dd60332a107e8
Jamie956 <752492509@qq.com>
2020-07-25 19:58:11 +0800

    termQuery

---
+++ src/main/java/es/test/Demo4.java
@@ -0,0 +1,36 @@
+package es.test;
+
+import es.utils.ESClient;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class Demo4 {
+    RestHighLevelClient client = ESClient.getClient();
+
+    @Test
+    public void termQuery() throws IOException {
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        builder.from(0);
+        builder.size(5);
+        builder.query(QueryBuilders.termQuery("birthday", "1970-12-12"));
+
+        SearchRequest request = new SearchRequest();
+        request.indices("lib");
+        request.types("user");
+        request.source(builder);
+
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+
+        for (SearchHit hit : response.getHits().getHits()) {
+            System.out.println(hit.getSourceAsMap());
+        }
+    }
+}
