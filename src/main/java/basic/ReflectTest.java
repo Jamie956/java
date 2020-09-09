a65b9bc6fd47040cdf50b277f194d67bfa85e9c6
Jamie956 <752492509@qq.com>
2020-09-09 11:23:39 +0800

    add

--- src/main/java/basic/ReflectTest.java
+++ src/main/java/basic/ReflectTest.java
@@ -1,31 +1,46 @@
 package basic;
 
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+
 public class ReflectTest {
     /**
      * 实例化构造一个class
      */
-    public static void test02() {
+    @Test
+    public void test02() {
         try {
             Class clz = Class.forName("basic.Person");
             clz.newInstance();
-
             clz.getDeclaredFields();
-
-
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
             e.printStackTrace();
         }
     }
 
+    @Test
+    public void asd() throws NoSuchFieldException, IllegalAccessException {
+        Object person = new Person();
+        Field personField = person.getClass().getDeclaredField("name");
+        personField.setAccessible(true);
+        personField.set(person, "111");
 
-    public static void main(String[] args) {
-        test02();
+        String name = (String) personField.get(person);
+    }
+
+    @Test
+    public void asdasd() throws NoSuchFieldException, IllegalAccessException {
+        Object person = new Person();
+        Field personField = person.getClass().getDeclaredField("name");
+        personField.setAccessible(true);
+        String name = (String) personField.get(person);
     }
 }
 
 class Person {
 
-    private String name;
+    private String name = "0";
 
     public Person() {
         System.out.println(1);
