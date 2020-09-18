c43f1126f896caf66926d42c95a9705c8eec1db0
Jamie956 <752492509@qq.com>
2020-09-18 21:04:26 +0800

    ok

--- src/main/java/basic/TestMain.java
+++ src/main/java/basic/TestMain.java
@@ -311,6 +311,44 @@ public class TestMain{
         new InitOrderB();
     }
 
+    private static int hash(String str) {
+        final int p = 16777619;
+        int hash = (int) 2166136261L;
+        for (int i = 0; i < str.length(); i++) {
+            hash = (hash ^ str.charAt(i)) * p;
+        }
+        hash += hash << 13;
+        hash ^= hash >> 7;
+        hash += hash << 3;
+        hash ^= hash >> 17;
+        hash += hash << 5;
+
+        if (hash < 0) {
+            hash = Math.abs(hash);
+
+        }
+        return hash;
+    }
+
+    /**
+     * 一致性hash环，根据用户ip路由到不同服务器地址（不带虚拟节点）
+     * 1. 初始化加载服务器地址，装在treemap里，key->hash, value->真实地址
+     * 2. 用户根据ip得到对应hash，treemap.get(hash(userIp)) 获取服务器列表，获取顺时针最靠近的值
+     */
+    @Test
+    public void consistentHashingWithoutVirtualNode() {
+        SortedMap<Integer, String> sortedMap = new TreeMap<>();
+
+        String[] servers = {"192.168.0.0:111", "192.168.0.1:111", "192.168.0.2:111", "192.168.0.3:111", "192.168.0.4:111"};
+
+        for (int i = 0; i < servers.length; i++) {
+            int hash = hash(servers[i]);
+            sortedMap.put(hash, servers[i]);
+        }
+
+        SortedMap<Integer, String> subMap = sortedMap.tailMap(hash("127.0.0.1:1111"));
+        String a = subMap.get(subMap.firstKey());
+    }
 }
 
 class InitOrder {
