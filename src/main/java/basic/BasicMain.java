145ddbb1b3b37c51961bfb2e818892341783cf8a
Jamie956 <752492509@qq.com>
2020-12-11 09:25:56 +0800

    up

--- src/main/java/basic/BasicMain.java
+++ src/main/java/basic/BasicMain.java
@@ -4,7 +4,6 @@ import basic.aenum.Color;
 import basic.aenum.Season;
 import basic.concurrency.SyncObject;
 import basic.entity.*;
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
 
@@ -12,8 +11,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.text.StringEscapeUtils;
 import org.junit.Test;
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
 import java.lang.reflect.Field;
 import java.net.ServerSocket;
@@ -849,58 +846,10 @@ public class BasicMain {
 //        list.sort(Comparator.comparingDouble((JSONObject e) -> e.getDoubleValue("value")).reversed());
     }
 
-//    @Test
-//    public void asdasf(){
-//        String a = HMACSHA256("", "");
-//    }
-//
-//
-//    public static String HMACSHA256(String data, String key) {
-//        if (data == null || key == null) {
-//            return "";
-//        }
-//        return HMACSHA256(data.getBytes(), key.getBytes());
-//    }
-//
-//    public static String HMACSHA256(byte[] data, byte[] key) {
-//        if (data == null || key == null) {
-//            return "";
-//        }
-//        try {
-//            SecretKeySpec secretKeySpec = new SecretKeySpec(key, "HmacSHA256");
-//            Mac instance = Mac.getInstance("HmacSHA256");
-//            instance.init(secretKeySpec);
-//            return bytes2hex(instance.doFinal(data));
-//        } catch (Exception e) {
-//            return "";
-//        }
-//    }
-//
-//    public static String bytes2hex(byte[] b) {
-//        if (b == null || b.length == 0) {
-//            return "";
-//        }
-//        StringBuilder sb = new StringBuilder();
-//        for (byte b2 : b) {
-//            String hexString = Integer.toHexString(b2 & -1);
-//            if (hexString.length() == 1) {
-//                sb.append('0');
-//            }
-//            sb.append(hexString);
-//        }
-//        return sb.toString().toUpperCase();
-//    }
 
+    //html 转义
     @Test
-    public void asd(){
-//        String template = "";
-        String template = null;
-
-        assert template != null;
-    }
-
-    @Test
-    public void asdasd(){
+    public void htmlConvert(){
         String str = "&nbsp;-&lt;-&gt;-&amp;-&quot;-&apos;";
 
         String unescapeHtml4 = StringEscapeUtils.unescapeHtml4(str);
