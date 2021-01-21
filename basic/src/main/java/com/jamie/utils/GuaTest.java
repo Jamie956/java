e98c3dea4eb918139edfe16ea04db108228705c8
Jamie956 <752492509@qq.com>
2021-01-21 14:26:41 +0800

    guava

---
+++ basic/src/main/java/com/jamie/utils/GuaTest.java
@@ -0,0 +1,33 @@
+package com.jamie.utils;
+
+import com.google.common.base.CaseFormat;
+import com.google.common.base.Converter;
+
+/**
+ * @Author: Zjm
+ * @Date: 2021/1/21 14:10
+ */
+public class GuaTest {
+    public static void main(String[] args) {
+        // 驼峰转下划线, userName -> user_name
+        Converter<String, String> converter = CaseFormat.LOWER_CAMEL.converterTo(CaseFormat.LOWER_UNDERSCORE);
+        // 输出: user_name
+        System.out.println(converter.convert("userName"));
+
+        // 驼峰转连接符, userName -> user-name
+        converter = CaseFormat.LOWER_CAMEL.converterTo(CaseFormat.LOWER_HYPHEN);
+        // 输出: user-name
+        System.out.println(converter.convert("userName"));
+
+        // 驼峰转首字符大写驼峰, userName -> UserName
+        converter = CaseFormat.LOWER_CAMEL.converterTo(CaseFormat.UPPER_CAMEL);
+        // 输出: UserName
+        System.out.println(converter.convert("userName"));
+
+        // 驼峰转大写下划线, userName -> USER_NAME
+        converter = CaseFormat.LOWER_CAMEL.converterTo(CaseFormat.UPPER_UNDERSCORE);
+        // 输出: USER_NAME
+        System.out.println(converter.convert("userName"));
+    }
+
+}
