655e00e568008953d893c3280bca543e56cabe9a
Jamie956 <752492509@qq.com>
2021-02-04 23:05:01 +0800

    up

--- bigdata/src/main/java/com/jamie/parsejson/JsonReduce2.java
+++ bigdata/src/main/java/com/jamie/parsejson/JsonReduce2.java
@@ -1,5 +1,7 @@
 package com.jamie.parsejson;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -11,6 +13,8 @@ public class JsonReduce2 extends Reducer<Text, Text, NullWritable, Text> {
 
     private MultipleOutputs<NullWritable, Text> multipleOutputs;
 
+    Text v = new Text();
+
     @Override
     protected void setup(Context context) throws IOException, InterruptedException {
         multipleOutputs = new MultipleOutputs<NullWritable, Text>(context);
@@ -18,10 +22,24 @@ public class JsonReduce2 extends Reducer<Text, Text, NullWritable, Text> {
 
     @Override
     protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        for (Text value : values) {
-            //重命名输出文件
-            multipleOutputs.write(NullWritable.get(), value, key.toString());
+
+        //如果是数组，拍平
+        if ("list".equals(key.toString())) {
+
+            for (Text value : values) {
+                JSONArray list = JSONObject.parseArray(value.toString());
+                for (Object ele : list) {
+                    v.set(ele.toString());
+                    multipleOutputs.write(NullWritable.get(), v, key.toString());
+                }
+            }
+        } else {
+            for (Text value : values) {
+                //重命名输出文件
+                multipleOutputs.write(NullWritable.get(), value, key.toString());
+            }
         }
+
     }
 
     @Override
