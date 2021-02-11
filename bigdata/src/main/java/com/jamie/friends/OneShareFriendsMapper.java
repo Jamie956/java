10212149a1ababd9f6d9fb89a966c03a0ed1b700
Jamie956 <752492509@qq.com>
2021-02-11 20:20:51 +0800

    up

--- bigdata/src/main/java/com/jamie/friends/OneShareFriendsMapper.java
+++ bigdata/src/main/java/com/jamie/friends/OneShareFriendsMapper.java
@@ -7,18 +7,21 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class OneShareFriendsMapper extends Mapper<LongWritable, Text, Text, Text> {
+    Text k = new Text();
+    Text v = new Text();
 
     @Override
     protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, Text>.Context context) throws IOException, InterruptedException {
         String line = value.toString();
-
         String[] fields = line.split(":");
-
         String person = fields[0];
         String[] friends = fields[1].split(",");
 
         for (String friend : friends) {
-            context.write(new Text(friend), new Text(person));
+            //好友作为key
+            k.set(friend);
+            v.set(person);
+            context.write(k, v);
         }
     }
 }
--- bigdata/src/main/java/com/jamie/friends/OneShareFriendsReducer.java
+++ bigdata/src/main/java/com/jamie/friends/OneShareFriendsReducer.java
@@ -7,10 +7,9 @@ import java.io.IOException;
 
 public class OneShareFriendsReducer extends Reducer<Text, Text, Text, Text> {
     @Override
-    protected void reduce(Text key, Iterable<Text> values, Context context)throws IOException, InterruptedException {
-
-        StringBuffer sb = new StringBuffer();
-        for(Text person: values){
+    protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        StringBuilder sb = new StringBuilder();
+        for (Text person : values) {
             sb.append(person).append(",");
         }
         context.write(key, new Text(sb.toString()));
