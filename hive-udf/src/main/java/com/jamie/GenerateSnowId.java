43fdacce4f08402a9e52562bbb0899993188a030
Jamie956 <752492509@qq.com>
2021-03-12 10:26:36 +0800

    up

--- hive-udf/src/main/java/com/jamie/GenerateSnowId.java
+++ hive-udf/src/main/java/com/jamie/GenerateSnowId.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.hive.ql.udf.UDFType;
 
 //表示函数返回值不重复
 @UDFType(deterministic = false)
-public class SnowId extends UDF {
+public class GenerateSnowId extends UDF {
     public long evaluate() {
         return IDGenerator.nextId();
     }
--- hive-udf/src/main/java/com/jamie/IDGenerator.java
+++ hive-udf/src/main/java/com/jamie/IDGenerator.java
@@ -1,7 +1,5 @@
 package com.jamie;
 
-import com.baomidou.mybatisplus.core.toolkit.Sequence;
-
 public class IDGenerator {
     private static Sequence sequence = new Sequence();
     public static long nextId(){
